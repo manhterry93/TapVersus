c1903c5475629f1312dc7188596400394b294297
Manh Pham-PC <phammanh.bk56@gmail.com>
2021-06-04 01:43:35 +0700

    Multi  player

--- app/src/main/java/pl/itto/tapversus/MultiPlayActivity.kt
+++ app/src/main/java/pl/itto/tapversus/MultiPlayActivity.kt
@@ -2,11 +2,38 @@ package pl.itto.tapversus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_multi_play.*
 
 class MultiPlayActivity : AppCompatActivity() {
+    var player1Point = 0
+    var player2Point = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_multi_play)
-        btn_play1
+        btn_play1.callBack = {
+            player1Point++
+            checkResult()
+        }
+        btn_play2.callBack = {
+            player2Point++
+            checkResult()
+        }
+    }
+
+    private fun checkResult() {
+        tv_player1_point.text = player1Point.toString()
+        tv_player2_point.text = player2Point.toString()
+        if (player1Point != player2Point && (player1Point > 50 || player2Point > 50)) {
+            btn_play1.callBack = null
+            btn_play2.callBack = null
+            if (player1Point > player2Point) {
+                tv_winner_1.visibility = View.VISIBLE
+                tv_winner_2.visibility = View.GONE
+            } else {
+                tv_winner_2.visibility = View.VISIBLE
+                tv_winner_1.visibility = View.GONE
+            }
+        }
     }
 }
